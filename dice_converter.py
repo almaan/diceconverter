6b7ad26b1aa4d814e336b541768f8e3f5a2cf8be
almaan <almaan@kth.se>
2021-07-02 08:44:23 +0200

    desgin file example

--- dice_converter.py
+++ dice_converter.py
@@ -12,31 +12,55 @@ from PIL import Image
 import os.path as osp
 import argparse as arp
 import sys
+from typing import Optional,Callable,Dict
 
 global FACEPATH
 
 FACEPATH = osp.join( osp.dirname(osp.realpath(__file__)),'faces')
 
-def make_dice_image(img, n):
+def sample_face(d: Dict[int,Image.Image])->Callable:
+    def fun(k):
+        if len(d[k]) > 1:
+            idx = np.random.choice(len(d[k]),size = 1)
+            return d[k][int(idx)]
+        else:
+            return d[k][0]
+    return fun
+
+def make_dice_image(img: Image.Image,
+                    n: int,
+                    design_file: Optional[str] = None
+                    ):
     """
     Takes an PIL.Image object, converts it to a dice-represenation
     and gives as output the rendered image as an PIL.image object
-    
+
     Arguments:
         - img : PIL.Imgae object (to be transformed)
         - n : pixel size to be used for each die
-    
+
     Returns:
         out : PIL.Image object (rendered dice-representation)
-    
+
     """
-    
+
     #dice size
     size = (n,n) 
     #dict holding image for each die
-    dice_dict = {x:Image.open(osp.join(FACEPATH,''.join(["00",str(x),'.png']))).resize(size) 
-                for x in range(1,7)}
-    #convert image to grayscale
+    if design_file is None:
+        dice_dict = {x:Image.open(osp.join(FACEPATH,
+                                           ''.join(["00",str(x),'.png']))).resize(size)
+                     for x in range(1,7)}
+        get_face = lambda x: dice_dict[x]
+    else:
+        import yaml
+        with open(design_file) as f:
+            dice_dict = yaml.load(f,Loader=yaml.FullLoader)
+        for k in dice_dict.keys():
+            for ii in range(len(dice_dict[k])):
+                dice_dict[k][ii] = Image.open(dice_dict[k][ii]).resize(size)
+        get_face = sample_face(dice_dict)
+
     img = img.convert('L')
     #image new output size
     newsize = (n*img.size[0],n*img.size[1])
@@ -50,8 +74,10 @@ def make_dice_image(img, n):
     #insert die-pictures to represent pixels
     for x in range(0,newsize[0],n):
         for y in range(0,newsize[1],n):
-            out.paste(dice_dict[digitized[int(x/n),int(y/n)]],(x,y))
-        
+            face_id = digitized[int(x/n),int(y/n)]
+            face = get_face(face_id)
+            out.paste(face,(x,y))
+
     return out
 
 prs = arp.ArgumentParser()
@@ -99,6 +125,16 @@ prs.add_argument("-k","--keep_size",
                  help = ' '.join(['keep current size of image',
                                  'and scale die images accordingly',
                                  ]))
+
+prs.add_argument("-df",
+                 "--design_file",
+                 required = False,
+                 default = None,
+                 help = ' '.join(["YAML design file that contains",
+                                  "paths to die images.",
+                                  ])
+                 )
+
     
 args = prs.parse_args()
 
@@ -115,15 +151,15 @@ if __name__ == '__main__':
     if args.keep_size:
         #allows user to keep size of the image using specied die size
         scaledsize = (int(1./args.die_size*img.size[0]),
-                  int(1./args.die_size*img.size[1]))
+                      int(1./args.die_size*img.size[1]))
     else:
         #resize image according to scaling factor provided
         scaledsize = (int(args.resize_factor*img.size[0]),
-                  int(args.resize_factor*img.size[1]))
+                      int(args.resize_factor*img.size[1]))
     
     img = img.resize(scaledsize)
     
-    img = make_dice_image(img, n = args.die_size)
+    img = make_dice_image(img, n = args.die_size,design_file =args.design_file)
     
     #check that proper output name is provided, change to .png otherwise
     try:
